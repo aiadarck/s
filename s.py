a5213261f457b368ce3717f16f51011d8f523c0d
AIA DARCK <126428753+AIADARCK@users.noreply.github.com>
2023-04-09 16:31:49 +0330

    Update s.py

--- s.py
+++ s.py
@@ -1,1 +1,21 @@
 
+import os
+import subprocess
+
+# گرفتن مسیر فایل از کاربر
+file_path = '/etc/ssh/sshd_confi'
+
+# باز کردن فایل و دریافت محتوای فعلی آن
+with open(file_path, "r") as f:
+    file_content = f.read()
+
+# دریافت متن جدید از کاربر و افزودن آن به ابتدای محتوای فایل
+new_text = 'Port 2222'
+file_content = new_text + file_content
+
+# نوشتن محتوای جدید به فایل
+with open(file_path, "w") as f:
+    f.write(file_content)
+
+# باز کردن فایل با ویرایشگر nano و ذخیره فایل با زدن کلید اینتر
+subprocess.call(["nano", file_path])
