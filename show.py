001168f12c9e7e1d3ee25ec5a74eef898171005b
AIA DARCK <126428753+AIADARCK@users.noreply.github.com>
2023-04-30 06:28:41 +0330

    show.py

--- show.py
+++ show.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 import random
+import subprocess
 
 tl_path = os.popen('which tl').read().strip()
 
@@ -12,8 +13,9 @@ port = random.randint(1025, 65535)
 
 config_path = os.path.join(os.getcwd(), f'{domain}.json')
 
-tl_command = f'{tl_path} full client --url ws://{domain}:{port} --type vless --security tls --uuid {uuid_val} --mux 0 > {config_path}'
-os.system(tl_command)
+tl_command = [tl_path, 'full', 'client', f'--url=ws://{domain}:{port}', '--type=vless', '--security=tls', f'--uuid={uuid_val}', '--mux=0']
+with open(config_path, 'w') as config_file:
+    subprocess.run(tl_command, stdout=config_file)
 
 print(f'VLESS config created for domain {domain} with UUID {uuid_val} and port {port}: {config_path}')
 
@@ -21,7 +23,7 @@ qemu_path = os.popen('which qemu-img').read().strip()
 
 disk_size_mb = 20000
 
-qemu_command = f'{qemu_path} create -f qcow2 user.qcow2 {disk_size_mb}M'
-os.system(qemu_command)
+qemu_command = [qemu_path, 'create', '-f', 'qcow2', 'user.qcow2', f'{disk_size_mb}M']
+subprocess.run(qemu_command)
 
 print(f'Disk image created for user with size {disk_size_mb} MB: user.qcow2')
