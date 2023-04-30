0b354a4a4c6abc6b787eaf082c42354bc04e6609
AIA DARCK <126428753+AIADARCK@users.noreply.github.com>
2023-04-30 06:02:20 +0330

    show.py

--- show.py
+++ show.py
@@ -2,6 +2,8 @@ import os
 import uuid
 import random
 
+tl_path = os.popen('which tl').read().strip()
+
 domain = 'meta.consolcbr.xyz'
 
 uuid_val = str(uuid.uuid4())
@@ -10,7 +12,6 @@ port = random.randint(1025, 65535)
 
 config_path = os.path.join(os.getcwd(), f'{domain}.json')
 
-tl_command = f'tl client --url ws://{domain}:{port} --type vless --security tls --uuid {uuid_val} --mux 0 > {config_path}'
+tl_command = f'{tl_path} client --url ws://{domain}:{port} --type vless --security tls --uuid {uuid_val} --mux 0 > {config_path}'
 os.system(tl_command)
 
-print(f'VLESS config created for domain {domain} with UUID {uuid_val} and port {port}: {config_path}')
