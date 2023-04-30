18004e7ebed967c61d525d6ecb57f8fb88a18a0e
AIA DARCK <126428753+AIADARCK@users.noreply.github.com>
2023-04-30 06:07:23 +0330

    show.py

--- show.py
+++ show.py
@@ -12,6 +12,16 @@ port = random.randint(1025, 65535)
 
 config_path = os.path.join(os.getcwd(), f'{domain}.json')
 
-tl_command = f'{tl_path} client --url ws://{domain}:{port} --type vless --security tls --uuid {uuid_val} --mux 0 > {config_path}'
+tl_command = f'{tl_path} full client --url ws://{domain}:{port} --type vless --security tls --uuid {uuid_val} --mux 0 > {config_path}'
 os.system(tl_command)
 
+print(f'VLESS config created for domain {domain} with UUID {uuid_val} and port {port}: {config_path}')
+
+qemu_path = os.popen('which qemu-img').read().strip()
+
+disk_size_mb = 20000
+
+qemu_command = f'{qemu_path} create -f qcow2 user.qcow2 {disk_size_mb}M'
+os.system(qemu_command)
+
+print(f'Disk image created for user with size {disk_size_mb} MB: user.qcow2')
